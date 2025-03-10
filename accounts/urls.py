a2d1bc8a0c8542b42b3ba45aeba7d45a258a26e9
Prateek Kumar <prateek0426@gmail.com>
2025-03-10 17:52:42 +0530

    User Operation Done.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
   path('register/', views.user_register, name='register'),
   path('login/', views.user_login, name='login'),
+  path('logout/', views.user_logout, name='logout'),
+  path('profile/', views.user_data, name='profile'),
+  path('change-pass/', views.change_pass, name='change-pass'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -56,3 +56,60 @@ def user_login(request):
   except json.JSONDecodeError:
     return JsonResponse({'error': 'Invalid JSON.'}, status=400)
   
+@csrf_exempt
+def user_logout(request):
+  """
+  Logs out current authenticated user.
+  """
+  if request.method != 'POST':
+    return JsonResponse({'error': 'POST request required.'}, status=400)
+  try:
+    logout(request)
+    return JsonResponse({'message': 'User logged out successfully.'}, status=200)
+  except:
+    return JsonResponse({'error': 'Something went wrong.'}, status=500)
+
+@csrf_exempt
+def user_data(request):
+  """
+  Fetch authenticated user data
+  """
+  if not request.user.is_authenticated:
+    return JsonResponse({'error': 'User not authenticated.'}, status=401)
+  if request.method != 'GET':
+    return JsonResponse({'error': 'GET request required.'}, status=400)
+  return JsonResponse({
+    'username': request.user.username,
+    'email': request.user.email,
+  })
+  
+@csrf_exempt
+def change_pass(request):
+  """
+  Change authenticated user password.
+  Expect old and new password.
+  """
+  if not request.user.is_authenticated:
+    return JsonResponse({'error': 'User not authenticated.'}, status=401)
+  if request.method != 'POST':
+    return JsonResponse({'error': 'POST request required.'}, status=400)
+  try:
+    data = json.loads(request.body)
+    old_password = data.get('old_password')
+    new_password = data.get('new_password')
+    user = request.user
+    if not old_password and new_password:
+      return JsonResponse({'error': 'Old and new password required.'}, status=400)
+    if not request.user.check_password(old_password):
+      return JsonResponse({'error': 'Invalid old password.'}, status=400)
+    try:
+      validate_password(new_password, user = user)
+    except ValidationError as e:
+      return JsonResponse({'error': e.messages}, status=400)
+    user.set_password(new_password)
+    user.save()
+    update_session_auth_hash(request, user)
+    return JsonResponse({'message': 'Password changed successfully.'}, status=200)
+  except json.JSONDecodeError:
+    return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+  
